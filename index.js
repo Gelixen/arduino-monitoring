00b8244e12458d47708b2e234f4b6cfc76c6aafc
Karolis šlipaitis <karolis.slipaitis@telesoftas.com>
2021-05-15 11:23:51 +0300

    Add metrics reading from serial port and pushing to graphite

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const lynx = require('lynx');
+const Lynx = require('lynx');
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline')
 
 let opt = {}; // opt.prefix = 'myPrefix';
-const metrics = new lynx('localhost', 8125, opt); // StatsD IP & Port
+const metrics = new Lynx('localhost', 8125, opt); // StatsD IP & Port
+const serialPort = new SerialPort('/dev/ttyACM0', 9600);
+const newLineSerialParser = serialPort.pipe(new Readline());
 
 metrics.increment('node.app.start'); // Send our first metric
 
-function updateTempGauge() {
-    metrics.gauge('arduino.sensor.temp', Math.random() * 10);
-    setTimeout(updateTempGauge, 1000);
-}
+serialPort.on('open', function () {
+    console.log('open');
+    newLineSerialParser.on('data', function(data) {
+        console.log('Incoming data: ' + data);
 
-updateTempGauge();
+        let [temperature, humidity, lightLevel] = data.split(',');
+
+        metrics.gauge('arduino.sensor.temperature', temperature);
+        metrics.gauge('arduino.sensor.humidity', humidity);
+        metrics.gauge('arduino.sensor.lightLevel', lightLevel);
+    });
+});
